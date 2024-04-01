e8053852884f707fdf534fe92093be9c43b3c7f0
Andrew Jennings <73889377+ajcoolcat@users.noreply.github.com>
2024-03-31 20:27:38 -0400

    404??

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('*', (req, res) => {
+  res.sendStatus(404)
+  res.send(__dirname + '/error/404.html');
 });
 
 module.exports = router;
