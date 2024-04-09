671252f8c1c542961dce745e0c77659e7b099bdc
Andrew Jennings <73889377+ajcoolcat@users.noreply.github.com>
2024-04-09 09:07:51 -0400

    Better spyware i mean logging

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const path = require('path');
+const path = require('node:path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const rfs = require('rotating-file-stream'); // version 2.x
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger('combined'));
+
+// Create a daily rotating log
+let accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'log')
+})
+
+app.use(logger('combined', { stream: accessLogStream }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
