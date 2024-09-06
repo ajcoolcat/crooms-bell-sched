50b21ba1bdf733559005e711d7975985bbef40e6
Andrew Jennings <73889377+ajcoolcat@users.noreply.github.com>
2024-09-06 13:55:22 -0400

    file management

---
+++ public/scripts/clippy/context-menu.js
@@ -0,0 +1,7 @@
+/*document.querySelector("clippy").addEventListener("context-menu", (e) => {
+    e.preventDefault();
+
+    const contextMenu = document.createElement("div");
+    contextMenu.classList.add("context-menu");
+    contextMenu.style.top = mousePos.y + "px";
+});*/
