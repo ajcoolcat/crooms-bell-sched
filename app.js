decfe8f12f040ec791414aeecfdb325000919512
thealmightyderpybird <73889377+ajcoolcat@users.noreply.github.com>
2024-10-11 08:32:08 -0400

    streamlined backend

--- app.js
+++ app.js
@@ -2,36 +2,11 @@ const express = require('express');
 const path = require('node:path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const rfs = require('rotating-file-stream'); // version 2.x
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-const app = express();
+export const app = express();
 
 app.use(logger('combined'));
-
-// Create a daily rotating log
-let accessLogStream = rfs.createStream('access.log', {
-    interval: '1d', // rotate daily
-    path: path.join(__dirname, 'log')
-})
-
-app.use(logger('combined', { stream: accessLogStream }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
-
-// CSP //
-/*
-        {
-          "key": "Content-Security-Policy",
-          "value": "default-src 'self' *.croomssched.tech croomssched.glitch.me *.weather.gov *.statuspage.io googleads.g.doubleclick.net; img-src *; media-src *; script-src 'self' *.croomssched.tech pagead2.googlesyndication.com croomssched.statuspage.io unsafe-inline; style-src: 'self' *.croomssched.tech unsafe-inline; frame-src: 'self' *.croomssched.tech *.kones.tech *.google.com; connect-src: pagead2.googlesyndication.com"
-        }*/
