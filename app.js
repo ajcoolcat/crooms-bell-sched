d48705c56b598095bcc799c83ab3e3e9710c3c17
thealmightyderpybird <73889377+ajcoolcat@users.noreply.github.com>
2024-10-12 13:43:13 -0400

    better bottom ad

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const path = require('node:path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-export const app = express();
+const app = express();
 
 app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
