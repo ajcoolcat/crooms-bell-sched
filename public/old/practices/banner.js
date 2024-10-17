6b6e4f6b06c7a7720e59a11b2913f9f6b5c9f8f4
Andrew Jennings <73889377+ajcoolcat@users.noreply.github.com>
2024-10-17 09:46:22 -0400

    something else

--- public/old/practices/banner.js
+++ public/old/practices/banner.js
@@ -1,5 +1,5 @@
 const bannerElement = document.createElement("div");
 bannerElement.id = "ad-banner";
-bannerElement.innerHTML = /*html*/`<a href="/" target="_blank"><img src="/favicon.ico" style="width: 17px; height: 17px;"> Made with CroomsSched</a>` +
-                          /*html*/`<style>#ad-banner {background-color: #eeeeee; width: 100vw; padding: 10px; position: fixed; bottom: 0px; left: 0px; user-select: none; font-family: SegUI, sans-serif;} #ad-banner a {text-decoration: none; color: black;} @font-face {font-family: SegUI; src: url("/SegUIVar.woff2");}</style>`;
+bannerElement.innerHTML = `<a href="/" target="_blank"><img src="/favicon.ico" style="width: 17px; height: 17px;"> Made by Crooms Bell Schedule</a>` +
+                          `<style>#ad-banner {background-color: #eeeeee; width: 100vw; padding: 10px; position: fixed; bottom: 0; left: 0; user-select: none; font-family: SegUI, sans-serif;} #ad-banner a {text-decoration: none; color: black;} @font-face {font-family: SegUI; src: url("https://cdn.croomssched.tech/fonts/SegUIVar.woff2");}</style>`;
 document.body.appendChild(bannerElement);
