624635ff591577aaddf00ad4d1cf4044a796e738
thealmightyderpybird <73889377+ajcoolcat@users.noreply.github.com>
2024-10-20 22:02:11 -0400

    removed links (again)

--- public/tools/feed-updates/submission.js
+++ public/tools/feed-updates/submission.js
@@ -1,17 +1,5 @@
 document.querySelector("#feed-form > footer > button").addEventListener("click", async () => {
     const feed = document.getElementById("feed-update");
-    const link = document.getElementById("feed-link");
-    const regex = /^https:?\/\/(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4])|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+[a-z\u00a1-\uffff]{2,}\.?)(?::\d{2,5})?(?:[\/?#]\S*)?$/i
-
-    if (!regex.test(link.value) && link.value !== "") {
-        const error = document.createElement("p");
-        error.innerText = "That is not a valid link.";
-        document.querySelector("main").appendChild(error);
-        error.classList.add("error");
-        link.addEventListener("keydown", () => {error.remove()});
-        document.querySelector("button").addEventListener("click", () => {error.remove()});
-        return;
-    }
 
     if (feed.value === "") {
         const error = document.createElement("p");
@@ -24,10 +12,6 @@ document.querySelector("#feed-form > footer > button").addEventListener("click",
     }
 
     let data = feed.value;
-
-    if (link.value !== "") {
-        data += " link: " + link.value;
-    }
 
     const request = new Request("https://api.croomssched.tech/feed", {
         method: "POST",
