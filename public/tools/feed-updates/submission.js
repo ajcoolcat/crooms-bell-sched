5bc23b4a236aa54773cfa1159e55763cb71466f5
thealmightyderpybird <73889377+ajcoolcat@users.noreply.github.com>
2024-11-03 09:10:08 -0500

    Update submission.js

--- public/tools/feed-updates/submission.js
+++ public/tools/feed-updates/submission.js
@@ -19,35 +19,41 @@ document.querySelector("#feed-form > footer > button").addEventListener("click",
             .join("");
     }
 
-    if (feed.value === "") {
+    function feedCreationError(message) {
         const error = document.createElement("p");
-        error.innerText = "Please enter a Feed Update.";
+        error.innerText = message;
         document.querySelector("main").appendChild(error);
         error.classList.add("error");
         feed.addEventListener("keydown", () => {error.remove()});
         document.querySelector("button").addEventListener("click", () => {error.remove()});
+    }
+
+    if (feed.value === "") {
+        feedCreationError("Please enter a Feed Update.");
         return;
     }
     if (link.value && !regex.test(link.value)) {
-        const error = document.createElement("p");
-        error.innerText = "The link entered is not valid. Please enter a valid link starting with \"https://\".";
-        document.querySelector("main").appendChild(error);
-        error.classList.add("error");
-        feed.addEventListener("keydown", () => {error.remove()});
-        document.querySelector("button").addEventListener("click", () => {error.remove()});
+        feedCreationError("The link entered is not valid. Please enter a valid link starting with \"https://\".");
         return;
     }
     let data = convertHTML(feed.value);
+    if (data.includes("<script") || data.includes("<iframe") || data.includes("<object") || data.includes("<embed") || data.includes("<template") || data.includes("<link")) {
+        feedCreationError("Scripts, embedded webpages, and embedded objects are not allowed.");
+        return;
+    }
+    if (data.includes("<img") || data.includes("<picture") || data.includes("<audio") || data.includes("<video") || data.includes("<source")) {
+        feedCreationError("Please add a link to your multimedia content.");
+        return;
+    }
+    if (data.includes("<a")) {
+        feedCreationError("Please use the link section to add a link.");
+        return;
+    }
     if (link.value) {
         data = "<a target=CBSHfeed href="+ link.value +">" + data + "</a>";
     }
 
     console.log(data);
-    const error = document.createElement("p");
-    error.innerHTML = data;
-    document.querySelector("main").appendChild(error);
-    error.classList.add("error");
-    document.querySelector("button").addEventListener("click", () => {error.remove()});
 
     const request = new Request("https://api.croomssched.tech/feed", {
         method: "POST",
@@ -64,11 +70,7 @@ document.querySelector("#feed-form > footer > button").addEventListener("click",
     const status = await response.json();
 
     if (status.data.error === "One of the following keys is missing or is empty in request body: 'data'") {
-        const error = document.createElement("p");
-        error.innerText = "Please enter a Feed Update.";
-        document.querySelector("main").appendChild(error);
-        error.classList.add("error");
-        document.querySelector("button").addEventListener("click", () => {error.remove()});
+        feedCreationError("Please enter a Feed Update.");
     } else if (status.data.error) {
         const error = document.createElement("p");
         error.innerText = status.data.error;
